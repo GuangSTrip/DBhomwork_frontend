4fde6422158091521daeee2a8168ee78a6e56117
GuangSTrip <1009550274@qq.com>
2022-11-23 13:06:14 +0800

    Initial

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+Vue.prototype.$axios = axios
+
+axios.defaults.baseURL = '10.128.48.151:8080';
 
 Vue.config.productionTip = false
 
